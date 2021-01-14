4733f3e0be88b6f4a10ab9cf790ccaf503203a40
zhaoaoxun <zhaoaoxun@gdu-tech.com>
2021-01-14 14:07:09 +0800

    机构管理-删除人员

--- src/views/backgroundManagement/institutionManagement.vue
+++ src/views/backgroundManagement/institutionManagement.vue
@@ -54,7 +54,7 @@
           >已选<span style="color: #1eb0fc">0</span>项</span
         >
         <div class="clean-btn">清空</div>
-        <div class="delete-btn">批量删除</div>
+        <div class="delete-btn" @click="deletePeopleClick">批量删除</div>
         <div class="add-btn" @click="addPeopleClick">添加</div>
       </div>
       <el-table
@@ -114,6 +114,7 @@
         ></el-pagination>
       </div>
     </div>
+
     <el-dialog
       :visible.sync="showAddPeople"
       :close-on-click-modal="clickfalse"
@@ -199,6 +200,29 @@
         <div class="cancel-btn" @click="addPeopleCancelClick">取消</div>
       </div>
     </el-dialog>
+
+    <el-dialog
+      :visible.sync="showDeleteTip"
+      append-to-body
+      :close-on-click-modal="false"
+      width="328px"
+      class="delete-tip-dial"
+    >
+      <div>
+        <div style="margin-top: 47px; height: 14px">
+          <div class="warning-img"></div>
+          <div class="title1">您确认删除这些数据吗？</div>
+        </div>
+        <div class="title2">
+          <span style="color: #ff0000">删除后无法撤销，</span>
+          <span style="color: #ffffff">您还要继续吗？</span>
+        </div>
+        <div style="margin-top: 21px">
+          <div class="confirm-btn" @click="deleteTipConfirmClick">确定</div>
+          <div class="cancel-btn" @click="deleteTipCancelClick">取消</div>
+        </div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -280,7 +304,8 @@ export default {
           id: 2,
           label: '女'
         }
-      ]
+      ],
+      showDeleteTip: false
     }
   },
   created () {
@@ -317,7 +342,9 @@ export default {
     handleSelectionChange () {},
 
     // 修改时触发
-    editPeopleClick () {},
+    editPeopleClick () {
+      this.showAddPeople = true
+    },
 
     // 查看时触发
     seePeopleClick () {},
@@ -338,6 +365,21 @@ export default {
     // 添加人员取消时触发
     addPeopleCancelClick () {
       this.showAddPeople = false
+    },
+
+    // 删除人员时触发
+    deletePeopleClick () {
+      this.showDeleteTip = true
+    },
+
+    // 删除人员确定时触发
+    deleteTipConfirmClick () {
+      this.showDeleteTip = false
+    },
+
+    // 删除人员取消时触发
+    deleteTipCancelClick () {
+      this.showDeleteTip = false
     }
   }
 }
@@ -680,6 +722,75 @@ export default {
         text-align: center;
         line-height: 30px;
         margin-right: 20px;
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+.delete-tip-dial.el-dialog__wrapper {
+  /deep/.el-dialog {
+    background-color: transparent;
+    box-shadow: none;
+    .el-dialog__header {
+      display: none;
+    }
+    .el-dialog__body {
+      display: inline-block;
+      padding: 0;
+      width: 100%;
+      height: 155px;
+      border: 1px solid #1EB0FC;
+      background-color: #121e3a;
+      .warning-img {
+        background-image: url("../../assets/images/backgroundManagement/warning.png");
+        width: 15px;
+        height: 14px;
+        display: inline-block;
+        vertical-align: top;
+        margin-left: 42px;
+      }
+      .title1 {
+        color: #ffffff;
+        font-size: 14px;
+        height: 14px;
+        line-height: 14px;
+        display: inline-block;
+        vertical-align: top;
+        margin-left: 13px;
+      }
+      .title2 {
+        margin-left: 70px;
+        height: 14px;
+        font-size: 14px;
+        line-height: 14px;
+        margin-top: 11px;
+      }
+      .confirm-btn {
+        width: 70px;
+        height: 24px;
+        background-color: #00cff9;
+        float: right;
+        margin-right: 19px;
+        border-radius: 4px;
+        color: white;
+        font-size: 12px;
+        text-align: center;
+        line-height: 24px;
+        cursor: pointer;
+      }
+      .cancel-btn {
+        width: 70px;
+        height: 24px;
+        background-color: transparent;
+        float: right;
+        margin-right: 10px;
+        color: #00cff9;
+        font-size: 12px;
+        text-align: center;
+        line-height: 24px;
+        border: 1px solid #00cff9;
+        border-radius: 4px;
         cursor: pointer;
       }
     }
