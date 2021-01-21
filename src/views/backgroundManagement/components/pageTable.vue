1e214a8db4b1325a84313af11a648bb34f114655
yc <yaochen85@163.com>
2021-01-21 10:00:06 +0800

    1.新增pageTable组件  2.搭建点/线/面资源设置表单UI

---
+++ src/views/backgroundManagement/components/pageTable.vue
@@ -0,0 +1,285 @@
+<template>
+  <div class="tableBox" :class="className">
+    <!-- 显示表格 -->
+    <el-table
+      ref="table"
+      :data="data"
+      style="width: 100%"
+      :height="tableHeight"
+    >
+      <el-table-column
+        v-if="checkBox"
+        :key="'selection'"
+        type="selection"
+        width="55"
+      />
+      <el-table-column
+        v-for="(item, index) in fieldList"
+        :key="index"
+        :prop="item.value"
+        :label="item.label"
+        :fixed="item.fixed"
+        align="center"
+        :width="item.width"
+        :min-width="item.minWidth || '100px'"
+        :show-overflow-tooltip="true"
+      >
+        <template v-slot="scope">
+          <el-switch
+            v-if="item.type === 'switch'"
+            v-model="scope.row[item.value]"
+          ></el-switch>
+          <span v-else>
+            {{ scope.row[item.value] }}
+          </span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        v-if="handle"
+        :key="'handle'"
+        :fixed="handle.fixed"
+        align="center"
+        :label="handle.label"
+        :width="handle.width"
+      >
+        <template v-slot="scope">
+          <div class="handerBox">
+            <template v-for="(item, index) in handle.btList">
+              <!-- 操作按钮 -->
+              <span
+                class="btn"
+                :key="index"
+                @click="handleClick(item.event, scope.row)"
+                >{{ item.label }}</span
+              >
+            </template>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页组件 -->
+    <template v-if="pager">
+      <el-pagination
+        style="text-align: center"
+        class="tablePagination"
+        popper-class="pageSelect"
+        :current-page.sync="listInfo.query.currentPage"
+        :page-sizes="listInfo.pageSizes"
+        :page-size="listInfo.query.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="listInfo.total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTable',
+  props: {
+    // 自定义类名
+    className: {
+      type: String
+    },
+    // 刷新
+    refresh: {
+      type: Number
+    },
+    // 表格高度
+    tableHeight: {
+      type: Number
+    },
+    // 获取数据的接口
+    api: {
+      type: Function
+    },
+    // 是否有选择框
+    checkBox: {
+      type: Boolean,
+      default: false
+    },
+    // 选中列表
+    checkedList: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    // 表格字段配置
+    fieldList: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    // 操作栏配置
+    handle: {
+      type: Object
+    },
+    // 是否分页
+    pager: {
+      type: Boolean,
+      default: true
+    },
+    // 重置当前页
+    initCurpage: {
+      type: Number
+    },
+    // 查询条件
+    query: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    // 列表数据
+    data: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      // 列表相关
+      listInfo: {
+        tableHeight: 0, // 表格最大高度
+        total: 0, // 总条数
+        pageSizes: [10, 20, 50, 100], // 分页数量列表
+        query: {
+          // 查询条件
+          currentPage: 1, // 当前页
+          pageSize: 10 // 每页条数
+        }
+      }
+    }
+  },
+  watch: {
+    initCurpage () {
+      this.listInfo.query.currentPage = 1
+    },
+    refresh () {
+      if (!this.api) return
+      this.getList(this.api)
+    }
+  },
+  methods: {
+    /**
+     *  派发按钮点击事件
+     */
+    handleClick (event, data) {
+      this.$emit('handleClick', event, data)
+    },
+    /**
+     *  处理参数
+     */
+    handleParams () {
+      const obj = {}
+      for (const key in this.query) {
+        if (this.query[key] || this.query[key] === 0) {
+          obj[key] = this.query[key]
+        }
+      }
+      // 如果不需要分页，则无分页相关参数
+      return this.pager ? { ...this.listInfo.query, ...obj } : obj
+    },
+    /**
+     *  得到数据
+     */
+    getList (api) {
+      // 每次调用接口时都自动绑定最新的数据
+      api(this.handleParams())
+        .then((res) => {
+          if (res.data.code === 0) {
+            // 使外面可以访问到表格数据
+            const arr = res.data.data.data
+            const paginator = res.data.data.paginator
+            this.$emit('update:data', arr)
+            if (this.pager) {
+              this.listInfo.total = paginator.totalCount
+              this.listInfo.query.currentPage = paginator.page
+              this.listInfo.query.pageSize = paginator.limit
+            }
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    handleSizeChange (val) {
+      const query = this.listInfo.query
+      query.pageSize = val // 每页条数
+      query.currentPage = 1 // 每页条数切换，重置当前页
+      this.getList(this.api)
+    },
+    handleCurrentChange (val) {
+      this.listInfo.query.currentPage = val // 当前页
+      this.getList(this.api)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tableBox {
+  /* 删除表格下横线 */
+  .el-table::before {
+    height: 0px;
+  }
+  .el-table {
+    color: #c5f3ff;
+    font-size: 16px;
+    background: transparent;
+    border: none;
+    /* 表格表头样式 */
+    /deep/.el-table__header-wrapper th {
+      color: #c5f3ff;
+      font-size: 16px;
+      height: 40px;
+      padding: 0;
+      background-color: rgba($color: #0b779e, $alpha: 0.66);
+    }
+    /* 表格每行高度*/
+    /deep/.el-table__body td {
+      height: 38px;
+      padding: 0;
+    }
+    /* 每行的背景颜色 */
+    /deep/.el-table__body td {
+      background-color: transparent;
+    }
+    /* 所有单元格颜色、无边框 */
+    /deep/td,
+    /deep/tr,
+    /deep/th {
+      border: none;
+      background-color: transparent;
+    }
+    /* 复选框样式 */
+    /deep/.el-checkbox__inner {
+      background-color: transparent;
+      border-color: #c5f3ff;
+      border-radius: 0px;
+    }
+    /*鼠标移入某行时的背景色*/
+    /deep/tbody tr:hover > td {
+      background-color: transparent;
+    }
+  }
+  .handerBox {
+    .btn {
+      display: inline-block;
+      width: 46px;
+      height: 26px;
+      background: #0b779e;
+      cursor: pointer;
+      font-size: 14px;
+      color: #fefefe;
+      line-height: 26px;
+    }
+    .btn:not(:last-child) {
+      margin-right: 18px;
+    }
+  }
+}
+</style>
--- src/views/backgroundManagement/pointResource.vue
+++ src/views/backgroundManagement/pointResource.vue
@@ -1,5 +1,245 @@
 <template>
-  <div>
-    pointResource
+  <div class="mapResBox">
+    <div class="title">地图数据 / {{ subTitle }}</div>
+    <div class="mainBox">
+      <div>{{ subTitle }}</div>
+      <div class="toolBox">
+        <span class="txt1">资源类型:</span>
+        <el-select v-model="res" placeholder="请选择" clearable class="select">
+          <el-option
+            v-for="(item, index) in resList"
+            :key="index"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+        <span class="txt2">所属机构:</span>
+        <el-select
+          v-model="organ"
+          placeholder="请选择"
+          clearable
+          class="select"
+        >
+          <el-option
+            v-for="(item, index) in organList"
+            :key="index"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+        <el-input
+          class="search"
+          v-model="searchStr"
+          placeholder="请输入资源名称/地址进行搜索"
+        ></el-input>
+        <div class="btn">
+          <img :src="searchIcon" />
+          <span>搜索</span>
+        </div>
+        <div class="btn resetBtn">
+          <img :src="resetIcon" />
+          <span>重置</span>
+        </div>
+      </div>
+      <div class="tablTool">
+        <div class="left">
+          <span>已选</span>
+          <span>4</span>
+          <span>项</span>
+          <span>清空</span>
+        </div>
+        <div class="right">
+          <span>添加</span>
+          <span>批量删除</span>
+        </div>
+      </div>
+      <PageTable
+        :checkBox="true"
+        :refresh="tableInfo.refresh"
+        :fieldList="tableInfo.fieldList"
+        :handle="tableInfo.handle"
+        :data.sync="tableInfo.data"
+        :tableHeight="600"
+        :query="query"
+        :api="getListApi"
+      ></PageTable>
+    </div>
   </div>
 </template>
+
+<script>
+import PageTable from './components/pageTable.vue'
+import { settingApi } from '@/api/setting'
+export default {
+  props: {
+    // 子标题
+    subTitle: {
+      default: '点资源设置',
+      type: String
+    }
+  },
+  data () {
+    return {
+      searchIcon: require('../../assets/images/backgroundManagement/searchIcon.png'),
+      resetIcon: require('../../assets/images/backgroundManagement/resetIcon.png'),
+      resList: [],
+      res: '',
+      organList: [],
+      organ: '',
+      searchStr: '',
+      // 表格相关
+      tableInfo: {
+        refresh: 1,
+        data: [
+          {
+            name: '江汉路',
+            addr: '武汉市江汉区江汉路',
+            type: '医院',
+            org: false,
+            sort: 1
+          }
+        ],
+        fieldList: [
+          { label: '设备类型', value: 'deviceTypeCode' },
+          { label: '设备名称', value: 'deviceName' },
+          { label: '设备编码', value: 'deviceCode' },
+          { label: '启用', value: 'deviceStatus', type: 'switch' },
+          { label: '在线状态', value: 'onlineStatus' }
+        ],
+        handle: {
+          label: '操作',
+          width: '130',
+          btList: [
+            {
+              label: '修改'
+            },
+            {
+              label: '查看'
+            }
+          ]
+        }
+      },
+      query: {
+        deviceCode: '',
+        deviceName: ''
+      }
+    }
+  },
+  components: {
+    PageTable
+  },
+  mounted () {
+    this.getList()
+  },
+  methods: {
+    /**
+     *  获取列表
+     */
+    getList () {
+      this.tableInfo.refresh = Math.random()
+    },
+    getListApi (params) {
+      return this.$axios.post(settingApi.queryDevicePage, params)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.mapResBox {
+  padding: 20px 31px 0px 20px;
+  .title {
+    font-size: 16px;
+    color: #ffffff;
+  }
+  .mainBox {
+    box-sizing: border-box;
+    padding: 12px 15px;
+    height: 814px;
+    background: #183157;
+    margin-top: 16px;
+    .toolBox {
+      display: flex;
+      align-items: center;
+      margin-top: 18px;
+      .txt1 {
+        margin-right: 17px;
+        margin-left: 12px;
+      }
+      .txt2 {
+        margin-right: 17px;
+        margin-left: 17px;
+      }
+      .select,
+      .search {
+        width: 150px;
+        height: 34px;
+        /deep/.el-input__inner {
+          background: rgba(9, 84, 109, 0.3);
+          border-radius: 0px;
+          border: 1px solid #1eb0fc;
+        }
+      }
+      .search {
+        width: 450px;
+        margin-left: 10px;
+        margin-right: 10px;
+      }
+      .btn {
+        box-sizing: border-box;
+        width: 80px;
+        height: 34px;
+        background: linear-gradient(120deg, #086384, #0b779e);
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        span {
+          color: #c5f3ff;
+        }
+        img {
+          margin-right: 6px;
+          margin-left: 9px;
+        }
+      }
+      .resetBtn {
+        margin-left: 10px;
+      }
+    }
+    .tablTool {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 10px;
+      margin-left: 41px;
+      margin-bottom: 13px;
+      .left {
+        span:nth-child(2) {
+          color: #1d9fe5;
+        }
+        span:nth-child(4) {
+          margin-left: 30px;
+          color: #1d9fe5;
+          border-bottom: solid 1px #1d9fe5;
+          cursor: pointer;
+        }
+      }
+      .right {
+        margin-left: 15px;
+        span {
+          text-align: center;
+          display: inline-block;
+          width: 54px;
+          height: 30px;
+          background: #1eb0fc;
+          cursor: pointer;
+          line-height: 30px;
+        }
+        span:nth-child(2) {
+          width: 96px;
+          margin-left: 12px;
+          background: #ff0000;
+        }
+      }
+    }
+  }
+}
+</style>
